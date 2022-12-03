244185c4c48c5d5f19a608309d19c2e2ce2ca2e9
Adam Schaal <amschaal@gmail.com>
2022-12-02 16:48:32 -0800

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+import PPMS from './PPMS.vue'
+var config = {
+  submission_tabs: [{ id: 'ppms', label: 'PPMS', component: PPMS }],
+}
+// , permissions: { ANY: ['ADMIN', 'STAFF', 'VIEW'] }
+export { config }
