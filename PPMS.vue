fa85179eb056640d25bd5c225db14eb67c175673
Adam Schaal <amschaal@gmail.com>
2022-12-06 14:41:59 -0800

    Showing services for Core

--- PPMS.vue
+++ PPMS.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    Config: {{config}}
-    Submission: {{submission.id}}
+
+    <q-table
+      title="Services"
+      :data="services"
+      row-key="Service id"
+      :dense="true"
+      :filter="filter"
+    >
+      <template v-slot:top-left>
+        <b>Services</b>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
   </div>
 </template>
 
@@ -10,11 +25,23 @@ export default {
   props: ['submission', 'config'],
   data () {
     return {
+      services: [],
+      filter: ''
     }
   },
   methods: {
   },
   mounted () {
+    this.$axios
+        .get(`/api/plugins/ppms/submissions/${this.submission.id}/services/`)
+        .then((response) => {
+          this.services = response.data.services
+        })
+        .catch((error) => {
+          if (error.response && error.response.status !== 404) {
+            this.$q.notify({message: 'There was an error retrieving the services.', type: 'negative'})
+          }
+        })
   },
   computed: {
   }
