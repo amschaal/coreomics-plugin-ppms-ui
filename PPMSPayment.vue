61642f2f17be2fea9ac364de35590438cd86d54d
Adam Schaal <amschaal@gmail.com>
2023-01-13 14:55:36 -0800

    Put payment component into plugin system

---
+++ PPMSPayment.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row">
+    <p class="col-sm-12 col-lg-12">Please ensure that your lab has an account set up at: <a href="https://ppms.us/ucdavis/">https://ppms.us/ucdavis/</a>. This email should be what the PI uses to log in to PPMS.</p>
+    <div class="col-sm-12 col-md-6 col-lg-3">
+      <q-input 
+        type="text"
+        bottom-slots
+        stack-label
+        v-model="value.pi_email"
+        label="PI Email/Login for PPMS"
+        :error="hasError('pi_email')"
+        :error-message="errorMessage('pi_email')"/>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { QSelect } from 'quasar'
+// import _ from 'lodash'
+
+export default {
+  props: ['value', 'editable', 'errors', 'modify'],
+  data () {
+    return {
+      payment: this.value
+    }
+  },
+  methods: {
+    errorMessage (field) {
+      if (this.errors && this.errors[field] && this.errors[field]) {
+        return this.errors[field].join ? this.errors[field].join(', ') : this.errors[field]
+      }
+      return ''
+    },
+    hasError (field) {
+      return this.errors && this.errors[field] !== undefined
+    }
+  },
+}
+</script>
+<style>
+.q-field {
+  padding: 3px !important;
+}
+</style>
--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 import PPMS from './PPMS.vue'
+import PPMSPayment from './PPMSPayment.vue'
 var config = {
   submission_tabs: [{ id: 'ppms', label: 'PPMS', component: PPMS }],
+  payment: PPMSPayment
 }
 // , permissions: { ANY: ['ADMIN', 'STAFF', 'VIEW'] }
 export { config }
